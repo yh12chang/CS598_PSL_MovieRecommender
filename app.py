34c300abc0b758697838648265ea121540823ae2
YongHun Chang <yh12.chang@gmail.com>
2024-12-15 01:43:11 -0600

    troubleshooting commit testing print

--- app.py
+++ app.py
@@ -362,10 +362,7 @@ if submit_rating:
                 if movie_idx >= len(top_movies_df):
                     break
                 movie_id = int(top_movies_df.index[movie_idx])
-
                 movie = movies_df[movies_df['movie_id'] == movie_id]
-                st.write(movie['title'])
-
                 with col:
                     st.image(movie['thumbnail_url'].values[0], use_container_width=True)
                     st.text(movie['title'].values[0])
