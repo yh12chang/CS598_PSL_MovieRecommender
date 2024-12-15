6d54c05007d43891a45b6a9259795dce789a0d15
YongHun Chang <yh12.chang@gmail.com>
2024-12-15 16:32:44 -0600

    new populartiy matrix

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ def myIBCF(newuser, similarity_matrix):
     pop_matrix_url = 'https://github.com/yh12chang/Cs_598_PSL_Project_4/raw/refs/heads/main/top_rated_movies.csv'
     pop_matrix_response = requests.get(pop_matrix_url)
     pop_csv = StringIO(pop_matrix_response.text)
-    popularity_data = pd.read_csv(pop_csv, index_col=0)
+    popularity_data = pd.read_csv(pop_csv)
 
     # Exclude the movies the user has already rated
     remaining_movies = popularity_data[~popularity_data['Title'].isin(movie_names[rated_indices])]
